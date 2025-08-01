3980652d1b08d3a3e834c9cd832f96d9c6dca3f5
Simple0x0 <Simple0xx0@gmail.com>
2025-08-01 19:32:41 +0000

    updated   firestore_client.py to handle empty collections

--- server/smartsiteapi/apps/database/firestore_client.py
+++ server/smartsiteapi/apps/database/firestore_client.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), '../../.env.yaml'), 'r') as f:
 
 # Path to your service account key (update this path as needed)
 SERVICE_ACCOUNT_PATH = os.path.join(
-    os.path.dirname(__file__), '..\\..\\..\\htechbtp-7f597-firebase-adminsdk-fbsvc-d74c398155.json'
+    os.path.dirname(__file__), '..\\..\\..\\htechbtp-7f597-firebase-adminsdk-fbsvc-dfb1001407.json'
 )
 
 # Initialize Firestore client
